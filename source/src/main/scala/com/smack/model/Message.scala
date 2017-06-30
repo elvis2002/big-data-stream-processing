9583d55bce38ef412ea4129988ed8886e59421a4
zhangbin130 <zhangbin130@wanda.cn>
2017-06-30 19:14:37 +0800

    init

--- source/src/main/scala/com/smack/model/Message.scala
+++
@@ -1,5 +0,0 @@
-package com.smack.model
-
-case class MsgEvent(namespace: String, serverName: String, msg: String)
-
-case class QueryEvent(namespace: String, serverName: String, start: Int, offset: Int)
